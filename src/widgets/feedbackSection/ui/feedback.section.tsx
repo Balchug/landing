3c2d83df7e3a9a785038beb9a5ffbb639f79f932
balchug <m.novikova@p-management.ru>
2023-01-26 11:01:52 +0300

    комментарии удалены

--- src/widgets/feedbackSection/ui/feedback.section.tsx
+++ src/widgets/feedbackSection/ui/feedback.section.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { Container } from '@mui/material'
 import { ISectionProps } from '~/shared/interfaces/section.interface'
