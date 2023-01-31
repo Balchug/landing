f01c6a3598edb12ba62b527ce94a241caba0496d
balchug <m.novikova@p-management.ru>
2023-01-31 14:59:26 +0300

    ChangePhone

--- src/widgets/footer/ui/contacts.tsx
+++ src/widgets/footer/ui/contacts.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contacts = () => (
   <div>
-    <div>Поддержка: +7 (999) 999-99-99</div>
+    <div>Поддержка: +7 (495) 970-85-17</div>
     <div>support@balchug-tech.ru</div>
   </div>
 )
