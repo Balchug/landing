24f30cec48b3e475b467a7c8d1b367238bb943d1
Menyalin Alexey <menyalin@icloud.com>
2023-09-01 14:57:44 +0300

    update about description

--- src/widgets/productSection/ui/product.section.tsx
+++ src/widgets/productSection/ui/product.section.tsx
@@ -5,8 +5,11 @@ import { ISectionProps } from '~/shared/interfaces/section.interface'
 import { SectionTitle } from '~/shared/ui'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Carousel } from 'react-responsive-carousel'
+
 import { features } from './features'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Slider from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
 
 const StyledHomeSection = styled.section`
   margin-top: 40px;
@@ -20,14 +23,11 @@ const containerStyles = {
   justifyContent: 'space-evenly',
 }
 
-const FeaturesWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  gap: 20px;
+const StyledCard = styled(Card)`
+  margin: 10px;
   padding: 10px;
-  overflow-x: auto;
-  justify-content: space-evenly;
+  margin-bottom: 20px;
+  height: 95%;
 `
 
 const PortalDescription = styled.div`
@@ -61,80 +61,97 @@ const DescriptionFooter = styled.div`
     margin-top: 30px;
   }
 `
-
-const HomeSection = ({ id, theme }: ISectionProps) => {
-  return (
-    <StyledHomeSection id={id} theme={theme}>
-      <Container sx={containerStyles}>
-        <PortalDescription>
-          БАЛЧУГ : Торговый портал - это современный инструмент для повышения
-          эффективности управления процессами торгового предприятия
-        </PortalDescription>
-        <MainModules>
-          <Typography variant="h5">
-            <b>Основные модули:</b>
-          </Typography>
-          <ul>Планирование</ul>
-          <ul>Листинг</ul>
-          <ul>Маркетинг</ul>
-          <ul>Ценообразование</ul>
-          <ul>Управленческие отчеты</ul>
-        </MainModules>
-        <DescriptionFooter>
-          И другие функции программы позволят в комплексе автоматизировать
-          задачи управленческого учета, анализа и планирования
-          <div className="description">
-            Программа разработана с использованием «1С:Библитека стандартных
-            подсистем»
-          </div>
-        </DescriptionFooter>
-      </Container>
-
-      <Container sx={containerStyles}>
-        <SectionTitle>Возможности</SectionTitle>
-        <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
-          <StaticImage src={'../../../images/features/01.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/02.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/03.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/04.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/05.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/06.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/07.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/08.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/09.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/10.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/11.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/12.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/13.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/14.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/15.JPG'} alt="Описание" />
-          <StaticImage src={'../../../images/features/16.JPG'} alt="Описание" />
-        </Carousel>
-        <FeaturesWrapper>
-          {features.map(block => (
-            <Card sx={{ width: '250px' }} key={block.title}>
-              <CardContent>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  sx={{ textAlign: 'center' }}
-                >
-                  {block.title}
-                </Typography>
-                <Typography
-                  sx={{ my: 1.5, textAlign: 'center' }}
-                  color="text.secondary"
-                >
-                  {block.content}
-                </Typography>
-                <Typography variant="body2"></Typography>
-              </CardContent>
-            </Card>
-          ))}
-        </FeaturesWrapper>
-      </Container>
-    </StyledHomeSection>
-  )
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
 }
+
+const HomeSection = ({ id, theme }: ISectionProps) => (
+  <StyledHomeSection id={id} theme={theme}>
+    <Container sx={containerStyles}>
+      <PortalDescription>
+        БАЛЧУГ : Торговый портал - это современный инструмент для повышения
+        эффективности управления процессами торгового предприятия
+      </PortalDescription>
+      <MainModules>
+        <Typography variant="h5">
+          <b>Основные модули:</b>
+        </Typography>
+        <ul>Планирование</ul>
+        <ul>Листинг</ul>
+        <ul>Маркетинг</ul>
+        <ul>Ценообразование</ul>
+        <ul>Управленческие отчеты</ul>
+      </MainModules>
+      <DescriptionFooter>
+        И другие функции программы позволят в комплексе автоматизировать задачи
+        управленческого учета, анализа и планирования
+        <div className="description">
+          <b>БАЛЧУГ : Торговый портал</b> разработан с использованием
+          <b> «1С:Библитека стандартных подсистем»</b>
+        </div>
+      </DescriptionFooter>
+    </Container>
+
+    <Container sx={containerStyles}>
+      <SectionTitle>Возможности</SectionTitle>
+      <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
+        <StaticImage src={'../../../images/features/01.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/02.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/03.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/04.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/05.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/06.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/07.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/08.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/09.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/10.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/11.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/12.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/13.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/14.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/15.JPG'} alt="Описание" />
+        <StaticImage src={'../../../images/features/16.JPG'} alt="Описание" />
+      </Carousel>
+      <Slider responsive={responsive}>
+        {features.map(block => (
+          <StyledCard key={block.title}>
+            <CardContent>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ textAlign: 'center' }}
+              >
+                {block.title}
+              </Typography>
+              <Typography
+                sx={{ my: 1.5, textAlign: 'center' }}
+                color="text.secondary"
+              >
+                {block.content}
+              </Typography>
+              <Typography variant="body2"></Typography>
+            </CardContent>
+          </StyledCard>
+        ))}
+      </Slider>
+    </Container>
+  </StyledHomeSection>
+)
 
 export default HomeSection
